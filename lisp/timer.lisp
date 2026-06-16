9f0ff62a8311d33c45f0dd44907fcd297c20f098
Gustavo Gabriel <gustavogabrielgabutti@gmail.com>
2026-06-16 17:40:14 -0300

    Delete lisp/timer.lisp

--- lisp/timer.lisp
+++
@@ -1,9 +0,0 @@
-(defun timer (timestamp-actual)
-  (let* ((duracion-total 219)
-         (momento-ciclo (mod timestamp-actual duracion-total)))
-    (cond
-      ((< momento-ciclo 120) 'en-rojo)
-      ((< momento-ciclo 121) 'en-amarillo)
-      ((< momento-ciclo 211) 'en-verde)
-      ((< momento-ciclo 216) 'en-amarillo)
-      (t 'amarillo-intermitente))))
