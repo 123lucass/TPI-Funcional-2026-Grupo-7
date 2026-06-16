ba9498e56864cd7205a9b100bf1bf9864b800c80
Gustavo Gabriel <gustavogabrielgabutti@gmail.com>
2026-06-16 11:51:18 -0300

    Create timer.lisp

---
+++ lisp/timer.lisp
@@ -0,0 +1,9 @@
+(defun timer (timestamp-actual)
+  (let* ((duracion-total 219)
+         (momento-ciclo (mod timestamp-actual duracion-total)))
+    (cond
+      ((< momento-ciclo 120) 'en-rojo)
+      ((< momento-ciclo 121) 'en-amarillo)
+      ((< momento-ciclo 211) 'en-verde)
+      ((< momento-ciclo 216) 'en-amarillo)
+      (t 'amarillo-intermitente))))
